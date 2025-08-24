02d36622f09156f1191a00a37a0cdce773a57110
PEDROZURITA1 <159149594+PEDROZURITA1@users.noreply.github.com>
2025-08-24 10:03:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '¡Bienvenido a mi aplicación Flask!'
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
-    return f'Bienvenido, {PEDRO}!'
+    return render_template('usuario.html', nombre=nombre)
 
 if __name__ == '__main__':
     app.run(debug=True)
